6ffaede860ed2b9b711ba44a83b4a55b34a6f8e9
juditAldeguer <jud.vicens@hotmail.es>
2022-01-21 11:53:59 +0100

    feat: add Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="home">
-    home...
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <div v-for="destination in destinations" :key="destination.name">
+        <router-link :to="destination.slug">
+          <h2>{{ destination.name }}</h2>
+        </router-link>
+        <figure>
+          <router-link :to="destination.name">
+            <img
+              :src="require(`../assets/${destination.image}`)"
+              :alt="destination.name"
+            />
+          </router-link>
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import store from "@/store.js";
 
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      destinations: store.destinations,
+    };
+  },
 };
 </script>
