429b9e75e90c10dccbecbea3809829e8d14bd2b2
juditAldeguer <jud.vicens@hotmail.es>
2022-01-21 13:09:20 +0100

    refactor: add component TheNavigation.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/brazil">Brazil</router-link> |
-      <router-link to="/hawaii">Hawaii</router-link> |
-      <router-link to="/panama">Panama</router-link> |
-      <router-link to="/jamaica">Jamaica</router-link>
-    </div>
+    <TheNavigation></TheNavigation>
     <router-view />
   </div>
 </template>
+
+
+<script>
+import TheNavigation from "./components/TheNavigation.vue";
+export default {
+  components: {
+    TheNavigation
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/brazil">Brazil</router-link>|
+    <router-link to="/hawaii">Hawaii</router-link>|
+    <router-link to="/panama">Panama</router-link>|
+    <router-link to="/jamaica">Jamaica</router-link>
+  </nav>
+</template>
+
+<style scoped>
+#nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
