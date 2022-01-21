a40412041461a781506c1b179de2dd19e925f10c
juditAldeguer <jud.vicens@hotmail.es>
2022-01-21 14:14:36 +0100

    feat: add route-name & component DestinationDetails

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,12 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "Jamaica" */ "../views/Jamaica.vue"),
   },
+  {
+    path: "/details",
+    name: "DestinationDetails",
+    component: () =>
+      import(/* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/DestinationDetails.vue
@@ -0,0 +1,7 @@
+<template>
+    <h2>
+       Hello details 
+    </h2>  
+    <!-- <p>The destination is: {{this.$route.params.id}} </p> -->
+</template> 
+
