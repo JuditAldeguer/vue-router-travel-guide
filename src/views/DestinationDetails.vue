8d9fe08aeafbc7f4e8396e571d4d58e3c35d40d4
juditAldeguer <jud.vicens@hotmail.es>
2022-01-24 10:47:53 +0100

    feat: add experiences in each details view

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,25 +1,52 @@
 <template>
-  <section class="destination">
-    <h1>{{ destination.name }}</h1>
-    <div class="destination-details">
-      <img :src="require(`../assets/${destination.image}`)" alt="destination" />
-      <p>{{ destination.description }}</p>
-    </div>
-  </section>
+  <div>
+    <section class="destination">
+      <h1>{{ destination.name }}</h1>
+      <div class="destination-details">
+        <img
+          :src="require(`../assets/${destination.image}`)"
+          alt="destination"
+        />
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+    <section class="experiences">
+      <h2>Top experiences in {{ destination.name }}</h2>
+      <div class="cards">
+        <div
+          class="card"
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+        >
+          <img
+            :src="require(`../assets/${experience.image}`)"
+            alt="experience.name"
+          />
+          <span class="card__text">
+            {{ experience.name }}
+          </span>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 import store from "@/store.js";
 export default {
   data() {
-    return {
-      destinationId: this.$route.params.id,
-    };
+    return {};
+  },
+  props: {
+    slug: {
+      type: String,
+      required: true,
+    },
   },
   computed: {
     destination() {
       return store.destinations.find(
-        (destination) => destination.id === this.destinationId
+        (destination) => destination.slug === this.slug
       );
     },
   },
@@ -43,5 +70,29 @@ p {
   margin: 0 40px;
   font-size: 20px;
   text-align: left;
+}
+
+.cards{
+    display: flex;
+    justify-content: space-between;
+}
+.card img {
+  max-height: 200px;
+}
+
+.card {
+  padding: 0 20px;
+  position: relative;
+}
+
+.card__text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+  text-decoration: none;
 }
 </style>
