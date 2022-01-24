225b706a334fe5276331cc948716a6beacd922cc
juditAldeguer <jud.vicens@hotmail.es>
2022-01-24 09:40:21 +0100

    refactor: delete unused path and views

--- src/router/index.js
+++ src/router/index.js
@@ -11,39 +11,6 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/brazil",
-    name: "Brazil",
-    component: () =>
-      import(/* webpackChunkName: "Brazil" */ "../views/Brazil.vue"),
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: () =>
-      import(/* webpackChunkName: "Hawaii" */ "../views/Hawaii.vue"),
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: () =>
-      import(/* webpackChunkName: "Panama" */ "../views/Panama.vue"),
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: () =>
-      import(/* webpackChunkName: "Jamaica" */ "../views/Jamaica.vue"),
-  },
-  {
     path: "/details:id",
     name: "DestinationDetails",
     component: () =>
